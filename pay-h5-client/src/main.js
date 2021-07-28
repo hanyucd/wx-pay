b9b2ed648d73c199cf7f588cf4e064c9b1c0585c
hanyucd <1807003141@qq.com>
2021-07-29 00:31:54 +0800

    feat: 安装移动端调试工具 vconsole

--- pay-h5-client/src/main.js
+++ pay-h5-client/src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router'
 import VueCookie from 'vue-cookie';
+import VConsole from 'vconsole';
 import './assets/css/reset.css';
 import './assets/css/common.css';
 
-Vue.use(VueCookie);
+// 移动端调试
+const vConsole = new VConsole();
+Vue.use(vConsole);
 
+Vue.use(VueCookie);
 Vue.config.productionTip = false;
 
 new Vue({
